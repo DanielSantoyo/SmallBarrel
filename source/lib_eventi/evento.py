2e816a737f7b71cb8ba77ca26eeec1c90984e4b7
DanielSantoyo <daniel.santoyo@gmx.com>
2016-06-21 17:35:22 +0200

    Add files via upload

--- source/lib_eventi/evento.py
+++ source/lib_eventi/evento.py
@@ -33,23 +33,63 @@ class Event(threading.Thread):
         self.sprite = None
     
     def try_move_random(self, x, y, z, verso):
-        '''carica il chunk ostacoli per verificare le posizioni valide nelle 8 celle vicine e muoversi'''
+        '''cerca nel chunk ostacoli per verificare le posizioni valide nelle 4 celle vicine e muoversi'''
         THREAD_LOCK.acquire()
+        
         su = 'X'
         giu= 'X'
         dx = 'X'
         sx = 'X'
         if y > 0:
             su = self.chunk_ostacoli.matrice[z][y-1][x]
+            if su == T_VOID:
+                if z > 0:
+                    if self.chunk_ostacoli.matrice[z-1][y-1][x] == FREE:
+                        su = T_DOWN
+            if su != EVENT:
+                if z < D_WORLD-1:
+                    if self.chunk_ostacoli.matrice[z+1][y][x] == T_VOID:
+                        if self.chunk_ostacoli.matrice[z+1][y-1][x] == FREE:
+                            su = T_UP
+
         if y < H_WORLD-1:
             giu = self.chunk_ostacoli.matrice[z][y+1][x]
+            if giu == T_VOID:
+                if z > 0:
+                    if self.chunk_ostacoli.matrice[z-1][y+1][x] == FREE:
+                        giu = T_DOWN
+            if giu != EVENT:
+                if z < D_WORLD-1:
+                    if self.chunk_ostacoli.matrice[z+1][y][x] == T_VOID:
+                        if self.chunk_ostacoli.matrice[z+1][y+1][x] == FREE:
+                            giu = T_UP
         if x > 0:
             sx = self.chunk_ostacoli.matrice[z][y][x-1]
+            if sx == T_VOID:
+                if z > 0:
+                    if self.chunk_ostacoli.matrice[z-1][y][x-1] == FREE:
+                        sx = T_DOWN
+            if sx != EVENT:
+                if z < D_WORLD-1:
+                    if self.chunk_ostacoli.matrice[z+1][y][x] == T_VOID:
+                        if self.chunk_ostacoli.matrice[z+1][y][x-1] == FREE:
+                            sx = T_UP
         if x < W_WORLD-1:
             dx = self.chunk_ostacoli.matrice[z][y][x+1]
+            if dx == T_VOID:
+                if z > 0:
+                    if self.chunk_ostacoli.matrice[z-1][y][x+1] == FREE:
+                        dx = T_DOWN
+            if dx != EVENT:
+                if z < D_WORLD-1:
+                    if self.chunk_ostacoli.matrice[z+1][y][x] == T_VOID:
+                        if self.chunk_ostacoli.matrice[z+1][y][x+1] == FREE:
+                            dx = T_UP
         ostacoli = [su,sx,dx,giu]
-        if ostacoli[verso] == ' ':
-            self.chunk_ostacoli.matrice[z][y][x] = ' '
+        
+        if ostacoli[verso] == FREE or ostacoli[verso] == T_DOWN or ostacoli[verso] == T_UP:
+            self.chunk_ostacoli.matrice[z][y][x] = FREE
+            
             if verso == 0:
                 self.verso = 1
                 self.y -= 1
@@ -62,5 +102,10 @@ class Event(threading.Thread):
             elif verso == 3:
                 self.verso = 0
                 self.y += 1
-            self.chunk_ostacoli.matrice[self.z][self.y][self.x] = 'E'
+            if ostacoli[verso] == T_UP:
+                self.z += 1
+            if ostacoli[verso] == T_DOWN:
+                self.z -= 1
+            self.chunk_ostacoli.matrice[self.z][self.y][self.x] = EVENT
+            
         THREAD_LOCK.release()
